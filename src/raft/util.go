e154251e607deffd41417626475916fcaf8d9ab9
junp <jpchen78@foxmail.com>
2021-05-27 22:18:31 +0800

    clean code

--- src/raft/util.go
+++ src/raft/util.go
@@ -1,7 +1,6 @@
 package raft
 
 import "log"
-import "math/rand"
 
 // Debugging
 var Debug = 1
@@ -11,15 +10,4 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 		log.Printf(format, a...)
 	}
 	return
-}
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-func rangeRand(max, min int) int {
-	return rand.Intn(max-min) + min
 }
