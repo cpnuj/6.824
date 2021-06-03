f8cacaba56e1b87a79ba006b0d42d6cc45e97933
junp <jpchen78@foxmail.com>
2021-06-03 22:59:39 +0800

    working on 2c

--- src/raft/test.sh
+++ src/raft/test.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 
+echo "Begin "$1
 for i in {1..20}
 do
 echo 'test round '$i' fails: '
-go test -run 2A | grep 'FAIL' | wc -l
+go test -run $1 | grep 'FAIL' | wc -l
 done
 
--- src/raft/test2B.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-for i in {1..20}
-do
-echo 'test round '$i' fails: '
-go test -run 2B | grep 'FAIL' | wc -l
-done
-
